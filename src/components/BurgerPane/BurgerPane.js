50b4627b8b2d23d5232269e87040d78fb708dabc
jyaymie <jamiesujiepark@gmail.com>
2022-06-12 23:56:34 -0700

    Allow the user to remove the most recent ingredient from the stack

--- src/components/BurgerPane/BurgerPane.js
+++ src/components/BurgerPane/BurgerPane.js
@@ -1,11 +1,18 @@
 import BurgerStack from '../BurgerStack/BurgerStack';
-import './BurgerPane.css'
+import './BurgerPane.css';
 
 function BurgerPane({ burgerStack, setBurgerStack }) {
+	function deleteIngredient() {
+		setBurgerStack(burgerStack.splice(1));
+	}
+
 	return (
-		<div className="burger-pane">
+		<div className='burger-pane'>
 			<BurgerStack burgerStack={burgerStack} />
 			<h3>Burger Stacking Area</h3>
+			<button type='click' onClick={deleteIngredient}>
+				Undo
+			</button>
 			<button
 				type='reset'
 				onClick={() => {
